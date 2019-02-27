10c10fec2a6b2db8f91a5f895eefd512c1faf8cc
Ramyapragathi <47380576+Ramyapragathi@users.noreply.github.com>
2019-02-27 15:38:39 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,11 @@
 import java.io.*;
-public class HelloWorld {
-	public static void main(String[] args) {
-		for(int i = 0; i < n; i++) {
-			System.out.println("Hello World!");
-		}
-	}
+public class Solution {
+    public static void main(String args[] ) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        int N = Integer.parseInt(line);
+        for (int i = 0; i < N; i++) {
+            System.out.println("hello world");
+        }
+    }
+}
